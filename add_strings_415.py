54fdd36341e76dec93b230593050f447edcd0a3f
mpyatetskyi <m.pyatetskyi@gmail.com>
2020-11-02 18:59:43 +0300

    415_add_strings

---
+++ add_strings_415.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/add-strings/
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+
+        num = str(int(num1) + int(num2))
+
+        return num
