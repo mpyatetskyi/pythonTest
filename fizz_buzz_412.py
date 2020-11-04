45d7c58e915c074868640fb75490c994c6a2331e
mpyatetskyi <m.pyatetskyi@gmail.com>
2020-11-04 17:28:01 +0200

    412_fizz_buzz

---
+++ fizz_buzz_412.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/fizz-buzz/submissions/
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        a = []
+        for val in range(1,n+1):
+            if val%3 == 0 and val%5 == 0:
+                a.append('FizzBuzz')
+            elif val%3 == 0:
+                a.append('Fizz')
+            elif val%5 == 0:
+                a.append('Buzz')
+            else:
+                a.append(str(val))
+        return a
