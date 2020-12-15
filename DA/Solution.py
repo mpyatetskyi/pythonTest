b8517a5eb344d6c566b0d6b1c033fd03e58c6119
mpyatetskyi <m.pyatetskyi@gmail.com>
2020-12-15 20:59:43 +0200

    Test task for DA

--- DA/Solution.py
+++ DA/Solution.py
@@ -5,11 +5,13 @@ class Solution():
 
 
     def my_func(self,mylist1,mylist2,mynum):
-         mysum: int = 0
+         mysum = None
          for i in mylist1:
              for j in mylist2:
-                 if i+j < mynum:
-                     mysum += (i+j)
+                 if i+j < mynum and mysum is None:
+                     mysum = (i+j)
+                 if i+j < mynum and i+j > mysum:
+                     mysum = i+j
                  else:
                      continue
 
@@ -17,31 +19,47 @@ class Solution():
 
 
 
-    def my_func2(self,mylist1,mylist2,mynum):
-        mysum = 0
+    def my_func_sorted(self,mylist1,mylist2,mynum):
+        mysum = None
         mylist1.sort()
         mylist2.sort()
         for i in mylist1:
             for j in mylist2:
+                if i+j < mynum and mysum is None:
+                    mysum = (i+j)
                 if i+j < mynum:
-                    mysum += (i+j)
+                    if i+j > mysum:
+                        mysum = (i+j)
+                    else:
+                        continue
                 else:
                     break
         return mysum
 
 
-    def my_func3(self,mylist1,mylist2,num):
-        mylist1.sort(reverse = True)
-        mylist2.sort(reverse = True)
+    def my_func_reverse_sorted(self,mylist1,mylist2,num):
+        mylist1.sort(reverse=True)
+        mylist2.sort(reverse=True)
+        mysum = None
+        for i in mylist1:
+            for j in mylist2:
+                if i+j > num:
+                    continue
+                elif i+j < num and mysum is None:
+                    mysum = i+j
+                    break
+                elif i+j < mysum:
+                    break
+                elif i+j > mysum:
+                    mysum = i+j
+        return mysum
 
-        if mylist1[0] + mylist2[0] < num:
-            return mylist1[0] + mylist2[0]
-
-
-a = [1,4,6,]
-b = [45,1,14,67]
-c = 100
+a = [-1,-4,6,-2]
+b = [-45,-1,-14,67]
+c = 67
 
 
 func = Solution()
-print(func.my_func3(a,b,c))
+print(func.my_func(a,b,c))
+print(func.my_func_sorted(a,b,c))
+print(func.my_func_reverse_sorted(a,b,c))
